a3e56380450dec102134df1729dfcd03e06f4404
Karin Touchie <ktouchie@gmail.com>
2015-12-14 10:52:40 +0100

    DOuble check and compile

--- Exercice5.java
+++ Exercice5.java
@@ -3,11 +3,11 @@ class Exercice5 {
     
     public static void main(String [] args) {  
 
-        int a[] = {2,5,3,1,4,-7,6};
+        int a[] = {-2,-1,0,1,2,3,4,5,6,7,8};
         boolean order = true;
         
         for (int i=0; i<(a.length-1); i++) {
-            if (a[i+1]>=a[i]) {
+            if (a[i+1]<=a[i]) {
             order = false;
             break;
             }
